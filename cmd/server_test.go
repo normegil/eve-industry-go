f97836f55860b63c89d99e264e119fabb5ccc7ff
Olivier Ory <mail@normegil.be>
2021-04-20 19:18:43 +0200

    Server - Review acceptance testing

--- cmd/server_test.go
+++ cmd/server_test.go
@@ -6,34 +6,10 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"os"
-	"os/exec"
 	"testing"
-	"time"
 )
 
 func TestHttpServer(t *testing.T) {
-	executablePath := os.Getenv("EXECUTABLE_PATH")
-	cmd := exec.Command(executablePath)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	if err := cmd.Start(); nil != err {
-		t.Fatal(fmt.Errorf("Could not start command '%s': %w", executablePath, err))
-	}
-	defer func() {
-		if err := cmd.Process.Signal(os.Interrupt); nil != err {
-			t.Fatal(fmt.Errorf("Could not stop command correctly '%s': %w", executablePath, err))
-		}
-	}()
-
-	go func() {
-		if err := cmd.Wait(); nil != err {
-			t.Fatal(fmt.Errorf("Error when running command '%s': %w", executablePath, err))
-		}
-	}()
-
-	time.Sleep(500 * time.Millisecond)
-
 	resp, err := http.Get("http://localhost:18080")
 	if err != nil {
 		t.Fatal(fmt.Errorf("Querying server: %w", err))
