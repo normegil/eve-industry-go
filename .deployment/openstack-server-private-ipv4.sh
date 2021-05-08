ba2d4803596c82511f78041d66db3b9f231d9023
Olivier Ory <mail@normegil.be>
2021-05-08 18:43:26 +0200

    Pipeline - Fix IPV4 script issue

--- .deployment/openstack-server-private-ipv4.sh
+++ .deployment/openstack-server-private-ipv4.sh
@@ -1,3 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 SERVER_NAME=$1
-openstack server show -c addresses --format json $SERVER_NAME | jq -r '.addresses.\"Ext-Net\" | .[]' | grep "^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$"
+IPV4_REGEX="^([0-9]{1,3}\.){3}[0-9]{1,3}$" 
+ADDRESSES=($(openstack server show -c addresses --format json $SERVER_NAME | jq -r '.addresses."Ext-Net" | .[]'))
+for address in "${ADDRESSES[@]}"
+do
+	if [[ $address =~ $IPV4_REGEX ]]; then
+		echo $address
+	fi
+done
